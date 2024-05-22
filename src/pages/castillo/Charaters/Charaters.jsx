97484b40b2c7ee3370e395e5b739a67d21202378
kelly103312 <kvquintana@REDCOLSA.LOCAL>
2024-05-22 07:50:45 -0500

    Logica del checkpoint

--- src/pages/castillo/Charaters/Charaters.jsx
+++ src/pages/castillo/Charaters/Charaters.jsx
@@ -9,17 +9,10 @@ export const Charaters = () => {
     const [avatar, setAvatar] = useState([0,0.5,-3]);
     const [bear, setBear] = useState([-1, 0, -80]);
     const {checkpoints, pointAchieved} = UseCheckpoints();
-    const [avatarPassedPoint, setAvatarPassedPoint] = useState(false); // Estado para controlar si el avatar ha pasado por cierto punto
 
     const bearRef = useRef()
     const avatarRef = useRef()
 
-    const handleAttack = () => {
-        console.log("El avatar 1 está atacando al avatar 2");
-        // Aquí puedes realizar cualquier lógica de ataque necesaria
-    };
-
-    // Verifica si el avatar ha pasado por cierto punto
     useEffect(() => {
         if(checkpoints){
             const position = JSON.parse(localStorage.getItem('position'));
