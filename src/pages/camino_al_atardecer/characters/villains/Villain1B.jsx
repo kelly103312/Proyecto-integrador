742a4b28cb0723556f626eb5cec10be3b82806d0
Hernan David Cisneros Vargas <hernandavidcisneros@gmail.com>
2024-06-02 14:37:44 -0500

    adding villans

---
+++ src/pages/camino_al_atardecer/characters/villains/Villain1B.jsx
@@ -0,0 +1,38 @@
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import React from "react";
+
+export default function Villain1B(props) {
+    // const Villain1ARef = useRef(null);
+    const { nodes, materials } = useGLTF('/assets/camino_al_atardecer/models/villains/Villain1B.glb');
+
+    // Verificar el contenido de nodes y materials
+    console.log(nodes, materials);
+
+    // const radius = 3.5;
+    // const speed = 1.5;
+
+    // useFrame((state, delta) => {
+    //     const elapsedTime = state.clock.getElapsedTime();
+    //     const angle = elapsedTime * speed;
+    //     const x = Math.cos(angle) * radius;
+    //     const z = Math.sin(angle) * radius;
+
+    //     Villain1ARef.current.position.set(props.position[0] + x, props.position[1], props.position[2] + z);
+    //     Villain1ARef.current.rotation.y = -angle;
+    // });
+
+    return (
+        <group {...props} dispose={null}>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.villain1B.geometry}
+            material={materials.villainMaterial}
+          />
+        </group>
+      )
+}
+
+useGLTF.preload('/assets/camino_al_atardecer/models/villains/Villain1B.glb');
--- src/pages/camino_al_atardecer/characters/villains/Villains.jsx
+++ src/pages/camino_al_atardecer/characters/villains/Villains.jsx
@@ -1,8 +1,13 @@
 import Villain1A from './Villain1A'
+import Villain1B from './Villain1B'
+import Villain1C from './Villain1C'
+import Villain1D from './Villain1D'
 
 export default function villains() {
     return <>
-        <Villain1A position={[0, 0, 100]} />
-    
+        <Villain1A />
+        <Villain1B />
+        <Villain1C />
+        <Villain1D />
     </>
 }
