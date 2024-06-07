69f1ebf1fc5e0d24f99035b475bbdfa13d74dadb
ErvinCaraval <ervin.caravali@correounivalle.edu.co>
2024-06-06 21:05:27 -0500

    se hace la funcionalidad completa de protegerse del ememigo,mientras este persigue.Ademas te protege de los obtaculos por cierto tiempo

--- src/context/ManagementLifes.jsx
+++ src/context/ManagementLifes.jsx
@@ -1,32 +1,42 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 
 // Crear el contexto de vidas
-
 export const LifesContext = createContext();
 
 export const useLifes = () => {
-    const context = useContext(LifesContext)
-    if(!context){
+    const context = useContext(LifesContext);
+    if (!context) {
         console.error("No existe el contexto");
         return;
     }
-  return context;
-}
+    return context;
+};
 
 // Proveedor de contexto
 export const LifesProvider = ({ children }) => {
     const [lifes, setLifes] = useState(3);
+    const [proteccionActiva, setProteccionActiva] = useState(false); // Estado para controlar si la protección está activa
 
     const restarLifes = () => {
-      setLifes(lifes - 1);
-      return lifes;
+        setLifes(prevLifes => {
+            if (!proteccionActiva) {
+                return prevLifes - 1;
+            } else {
+                return prevLifes; // Si la protección está activa, no restar más vidas
+            }
+        });
     };
 
-    
-  return (
-    <LifesContext.Provider value={({ lifes, restarLifes })}>
-      {children}
-    </LifesContext.Provider>
-  );
-};
+    const mantenerVidas = () => {
+        setProteccionActiva(true); // Activar la protección
+        setTimeout(() => {
+            setProteccionActiva(false); // Desactivar la protección después de 60 segundos
+        }, 60000); // 60000 ms = 60 segundos
+    };
 
+    return (
+        <LifesContext.Provider value={{ lifes, restarLifes, mantenerVidas }}>
+            {children}
+        </LifesContext.Provider>
+    );
+};
