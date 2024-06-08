46b361553bd35baa0fa29cb0ba5329ad31a4ab52
ErvinCaraval <ervin.caravali@correounivalle.edu.co>
2024-06-07 21:30:36 -0500

    todo

--- src/routes/RoutesGame.jsx
+++ src/routes/RoutesGame.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { MontanasHielo } from '../pages/montanas_hielo/MontanasHielo'
-import  CuevaEncantada  from '../pages/cueva_encantada/Cueva_Encantada'
+
 import Level1 from '../pages/level1/Level1'
 import Login from '../pages/login/Login'
 import Level2 from '../pages/level2/Level2'
@@ -16,7 +16,7 @@ export const RoutesGame = () => {
           <Route path="/level2" element={<Level2 />} />
           <Route path="/castillo" element={<Castillo/>} />
           <Route path="/montanas_hielo" element={<MontanasHielo/>} />
-          <Route path="/cueva_encantada" element={<CuevaEncantada/>} />
+          
         </Routes>
     </BrowserRouter>
   )
