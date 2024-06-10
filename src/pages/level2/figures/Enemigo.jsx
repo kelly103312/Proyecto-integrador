211cbdddfa84955635f4cfc28e8547b1afa2763d
ErvinCaraval <ervin.caravali@correounivalle.edu.co>
2024-06-09 23:58:06 -0500

    listo animacion del dragon

--- src/pages/level2/figures/Enemigo.jsx
+++ src/pages/level2/figures/Enemigo.jsx
@@ -11,7 +11,7 @@ export function Model(props) {
   const { lifes, restarLifes } = useLifes(); // Acceso al contexto de vidas
   const { nodes, materials, animations } = useGLTF('./assets/level2/models/players/enemigo.glb');
   const { actions } = useAnimations(animations, group);
-  const [currentAction, setCurrentAction] = useState('Walking'); // Animación inicial
+  const [currentAction, setCurrentAction] = useState('Rest'); // Animación inicial
   const [screamTriggered, setScreamTriggered] = useState(false); // Estado para controlar si ya se restó una vida
 
   useEffect(() => {
@@ -30,10 +30,15 @@ export function Model(props) {
     const modelPosition = group.current.position.clone();
     const direction = avatarPosition.clone().sub(modelPosition).normalize(); // Dirección hacia el avatar
 
+    // Calcular la distancia al avatar
+    const distanceToAvatar = modelPosition.distanceTo(avatarPosition);
+
+    // Ajustar la velocidad de movimiento en función de la distancia al avatar
+    const speed = distanceToAvatar < 1 ? 0.01 : 0.05; // Movimiento lento cuando está cerca
+
     // Actualizar la posición del modelo
-    const speed = 0.05; // Velocidad de movimiento
     const movement = direction.clone().multiplyScalar(speed);
-    modelPosition.add(movement); 
+    modelPosition.add(movement);
     group.current.position.copy(modelPosition);
 
     // Calcular y aplicar la rotación hacia el avatar
@@ -43,44 +48,44 @@ export function Model(props) {
     targetQuaternion.setFromEuler(euler);
     group.current.quaternion.slerp(targetQuaternion, 0.1); // Interpolar suavemente hacia la nueva rotación
 
-    // Cambiar a la animación "Scream" si está cerca del avatar y restar vidas
-    if (modelPosition.distanceTo(avatarPosition) < 1) {
+    // Cambiar a la animación "RideIdle" si está cerca del avatar y restar vidas
+    if (distanceToAvatar < 1) {
       if (!screamTriggered) {
-        setCurrentAction('Scream');
+        setCurrentAction('Rest');
         setScreamTriggered(true);
         if (lifes > 0) {
           restarLifes();
         }
       }
     } else {
-      setCurrentAction('Walking');
+      setCurrentAction('RideIdle');
       setScreamTriggered(false); // Reiniciar el estado cuando el enemigo se aleja
     }
   });
 
   return (
     <group ref={group} {...props} dispose={null}>
-    <group name="Scene">
-      <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]} scale={0.122}>
-        <group name="root">
-          <group name="GLTF_SceneRootNode" rotation={[Math.PI / 2, 0, 0]}>
-            <group name="Object_5_232">
-              <group name="GLTF_created_0">
-                <skinnedMesh
-                  name="Object_7"
-                  geometry={nodes.Object_7.geometry}
-                  material={materials.fei_long}
-                  skeleton={nodes.Object_7.skeleton}
-                />
-                <group name="Object_8_229" />
-                <primitive object={nodes.GLTF_created_0_rootJoint} />
+      <group name="Scene">
+        <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]} scale={0.10}>
+          <group name="root">
+            <group name="GLTF_SceneRootNode" rotation={[Math.PI / 2, 0, 0]}>
+              <group name="Object_5_232">
+                <group name="GLTF_created_0">
+                  <skinnedMesh
+                    name="Object_7"
+                    geometry={nodes.Object_7.geometry}
+                    material={materials.fei_long}
+                    skeleton={nodes.Object_7.skeleton}
+                  />
+                  <group name="Object_8_229" />
+                  <primitive object={nodes.GLTF_created_0_rootJoint} />
+                </group>
               </group>
             </group>
           </group>
         </group>
       </group>
     </group>
-  </group>
   );
 }
 
